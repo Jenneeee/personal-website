1bea8941f3c9c6e4cb0a91f04558369395b8a27d
Jenneeee <jenne@cattoor.net>
2022-01-03 14:14:36 +0100

    Using responsive images

--- src/components/case.js
+++ src/components/case.js
@@ -7,7 +7,7 @@ import './case.scss'
 export default function Case() {
   return (
   <section className="case-wrapper">
-    <StaticImage src="../images/1.png" alt="A rectangle" />
+    <StaticImage src="../images/case1.jpg" alt="A rectangle" />
     <p>Google Doodle - School Project</p>
     <h3>01: Making a Google Doodle</h3>
     <Link className="button" to="/"><div className="button-arrow"><Arrow /></div><span className="button-text">Vieuw case</span></Link>
