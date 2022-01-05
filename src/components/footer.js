105cdb62f4ac50bc235423bfb79537dc3b75ef00
Jenneeee <jenne@cattoor.net>
2022-01-05 18:09:32 +0100

    Finished footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,21 +6,11 @@ import '../styles/components/footer.scss'
 export default function Footer() {
   return (
   <footer className="footer">
-    <div className="footer-item">
-      <p>Social media</p>
       <div className='socials-footer'>
         <a href="https://www.instagram.com/jennecattoor" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram}/><span className='hidden'>Instagram</span></a>
         <a href="https://dribbble.com/Cattoor" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faDribbble} /><span className='hidden'>Dribbble</span></a>
         <a href="https://github.com/Jenneeee" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /><span className='hidden'>GitHub</span></a>
       </div>
-    </div>
-    <div className="footer-item">
-      <p>Email</p>
-      <p>jenne@cattoor.net</p>
-    </div>
-    <div className="footer-item">
-      <p>Copyright</p>
       <p>© 2022 Jenne Cattoor</p>
-    </div>
  </footer>)
 }
