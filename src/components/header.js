907416604a337d31ffb2aa58af3e05141d66850c
Jenneeee <jenne@cattoor.net>
2022-03-25 11:20:50 +0100

    Adding animations to index page and fixing responsive issues

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,28 @@
-import React from "react"
+import React, {useState} from "react"
 import { Link } from 'gatsby'
 import * as styles from'./header.module.scss'
 
 export default function Header () {
+  const [navbarOpen, setNavbarOpen] = useState(false)
+
+  const handleToggle = () => {
+    setNavbarOpen(prev => !prev)
+  }
 
   return (
-  <header
-    className={styles.header}>
-    <h1><Link className={styles.name} to='/'>Jenne Cattoor</Link></h1>
-    <nav className={styles.navigation}>
-        <ul className={styles.navList}>
-            <li className={styles.navItem}><Link to='/' className={styles.navLink} activeClassName={styles.navLinkActive}>Portfolio</Link></li>
-            <li className={styles.navItem}><Link to='/about' className={styles.navLink} activeClassName={styles.navLinkActive}>About</Link></li>
-            <li className={styles.navItem}><Link to='/contact' className={styles.navLink} activeClassName={styles.navLinkActive}>Contact</Link></li>
-        </ul>
-    </nav>
+  <header className={styles.header}>
+      <h1><Link className={styles.name} to='/'>Jenne Cattoor</Link></h1>
+      <nav className={styles.navigation}>
+          <ul className={styles.navList}>
+              <li className={styles.navItem}><Link to='/' className={styles.navLink} activeClassName={styles.navLinkActive}>Portfolio</Link></li>
+              <li className={styles.navItem}><Link to='/about' className={styles.navLink} activeClassName={styles.navLinkActive}>About</Link></li>
+              <li className={styles.navItem}><Link to='/contact' className={styles.navLink} activeClassName={styles.navLinkActive}>Contact</Link></li>
+         </ul>
+     </nav>
+    <button className={styles.hamburger} onClick={handleToggle}>
+            <span className={styles.hidden}>Toggle Menu</span>
+            <span className={styles.bar}></span>
+            <span className={styles.bar}></span>
+      </button>
  </header>)
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,18 +6,23 @@ import Header from '../components/header'
 import Links from '../components/links'
 import Footer from '../components/footer'
 import * as styles from './index.module.scss'
+import { motion } from 'framer-motion'
 
 export default function Index () {
   return (
   <>
     <Seo title="Multidisciplinary designer and front-end developer" />
     <Header />
-    <div className={styles.wrapper}>
+    <motion.div 
+      initial={{y:30, opacity: 0}}
+      animate={{y: 0, opacity: 1}}
+      transition={{duration: .4}}
+      className={styles.wrapper}>
       <div className={styles.intro}>
         <h2 className={styles.introText}>A multidisciplinary Designer & <br className={styles.brDeskktop}/>Front-end Developer <br className={styles.brDeskktop}/>based in Belgium.</h2>
         <Line />
       </div>
-    </div>
+    </motion.div>
     <Links />
     <Footer />
   </>)
