01b274a70436264f81139d5f45c81a3f92976dfe
Jenneeee <jenne@cattoor.net>
2022-07-27 21:30:19 +0200

    Adding max-width to all pages

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,16 +3,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faInstagram, faDribbble, faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons'
 import * as styles from './footer.module.scss'
 
-export default function Footer () {
+export default function Footer() {
   const year = new Date().getFullYear();
   return (
-  <footer className={styles.footer}>
-      <div className={styles.socials}>
-        <a href="https://www.instagram.com/jennecattoor" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram}/><span className={styles.hidden}>Instagram</span></a>
-        <a href="https://dribbble.com/Cattoor" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faDribbble} /><span className={styles.hidden}>Dribbble</span></a>
-        <a href="https://github.com/Jenneeee" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /><span className={styles.hidden}>GitHub</span></a>
-        <a href="https://www.linkedin.com/in/jennecattoor" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedinIn} /><span className={styles.hidden}>LinkedIn</span></a>
+    <footer className={styles.footer}>
+      <div className={styles.wrapper}>
+        <div className={styles.socials}>
+          <a href="https://www.instagram.com/jennecattoor" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} /><span className={styles.hidden}>Instagram</span></a>
+          <a href="https://dribbble.com/Cattoor" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faDribbble} /><span className={styles.hidden}>Dribbble</span></a>
+          <a href="https://github.com/Jenneeee" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /><span className={styles.hidden}>GitHub</span></a>
+          <a href="https://www.linkedin.com/in/jennecattoor" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedinIn} /><span className={styles.hidden}>LinkedIn</span></a>
+        </div>
+        <p>© {year} Jenne Cattoor</p>
       </div>
-      <p>© { year } Jenne Cattoor</p>
- </footer>)
+    </footer>)
 }
