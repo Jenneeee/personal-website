1a8b21f085bfee1fc334681164350e97be8b3250
Jenneeee <jenne@cattoor.net>
2022-07-28 14:51:40 +0200

    Fixing lighthouse issues

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram, faDribbble, faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faDribbble, faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons'
 import * as styles from './footer.module.scss'
 
 export default function Footer() {
@@ -9,10 +9,9 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div className={styles.wrapper}>
         <div className={styles.socials}>
-          <a href="https://www.instagram.com/jennecattoor" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} /><span className={styles.hidden}>Instagram</span></a>
-          <a href="https://dribbble.com/Cattoor" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faDribbble} /><span className={styles.hidden}>Dribbble</span></a>
-          <a href="https://github.com/Jenneeee" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /><span className={styles.hidden}>GitHub</span></a>
-          <a href="https://www.linkedin.com/in/jennecattoor" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedinIn} /><span className={styles.hidden}>LinkedIn</span></a>
+          <a href="https://dribbble.com/Cattoor" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faDribbble} aria-label="Dribbble" /><span className={styles.hidden}>Dribbble</span></a>
+          <a href="https://github.com/Jenneeee" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} aria-label="GitHub" /><span className={styles.hidden}>GitHub</span></a>
+          <a href="https://www.linkedin.com/in/jennecattoor" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedinIn} aria-label="LinkedIn" /><span className={styles.hidden}>LinkedIn</span></a>
         </div>
         <p>© {year} Jenne Cattoor</p>
       </div>
--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,8 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import { Link } from 'gatsby'
-import * as styles from'./header.module.scss'
+import * as styles from './header.module.scss'
 
-export default function Header () {
+export default function Header() {
   const [navbarOpen, setNavbarOpen] = useState(false)
 
   const handleToggle = () => {
@@ -11,19 +11,19 @@ export default function Header () {
   }
 
   return (
-  <header className={styles.header}>
+    <header className={styles.header}>
       <h1><Link className={styles.name} to='/'>Jenne Cattoor</Link></h1>
       <nav className={styles.navigation}>
-          <ul className={styles.navList}>
-              <li className={styles.navItem}><Link to='/' className={styles.navLink} activeClassName={styles.navLinkActive}>Portfolio</Link></li>
-              <li className={styles.navItem}><Link to='/about' className={styles.navLink} activeClassName={styles.navLinkActive}>About</Link></li>
-              <li className={styles.navItem}><Link to='/contact' className={styles.navLink} activeClassName={styles.navLinkActive}>Contact</Link></li>
-         </ul>
-     </nav>
-    <button className={styles.hamburger} onClick={handleToggle}>
-            <span className={styles.hidden}>Toggle Menu</span>
-            <span className={styles.bar}></span>
-            <span className={styles.bar}></span>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}><Link to='/' className={styles.navLink} activeClassName={styles.navLinkActive}>Portfolio</Link></li>
+          <li className={styles.navItem}><Link to='/about' className={styles.navLink} activeClassName={styles.navLinkActive}>About</Link></li>
+          <li className={styles.navItem}><Link to='/contact' className={styles.navLink} activeClassName={styles.navLinkActive}>Contact</Link></li>
+        </ul>
+      </nav>
+      <button className={styles.hamburger} aria-label="Toggle menu" onClick={handleToggle}>
+        <span className={styles.hidden}>Toggle Menu</span>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
       </button>
- </header>)
+    </header>)
 }
